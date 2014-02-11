3176dab4f09c850ccd9922f84c01a2dec8c42e95
Jon Lebensold <jon@lebensold.ca>
2014-02-12 00:55:49 +0100

    deployment hacking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,15 @@ WieWeb::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = flase
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # change this later:
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
