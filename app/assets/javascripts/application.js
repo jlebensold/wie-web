b537685122894985fba9cbce0e93ba2899410b5f
Jon Lebensold <jon@lebensold.ca>
2014-02-12 00:25:15 +0100

    added multimap support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,11 @@
 //= require jquery
+//= require 'jquery-jvectormap-1.2.2.min'
+//= require 'jquery-jvectormap-europe-mill-en'
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require underscore.min
 //= require turbolinks
+//= require 1945
+//= require 1994
+//= require 2013
 //= require_tree .
-//= require 'jquery-jvectormap-1.2.2.min'
-//= require 'jquery-jvectormap-europe-mill-en'
