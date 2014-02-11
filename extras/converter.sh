694a0146c8797a7097fbc69d74d391c49d02a476
Jon Lebensold <jon@lebensold.ca>
2014-02-12 00:29:05 +0100

    added converter script

---
+++ extras/converter.sh
@@ -0,0 +1,5 @@
+python2 converter.py --width 1000 --viewport "-24.1 25.1 60.3 71.4" --longitude0 11.4 --name 2013 --projection mill --language en --country_name_index 11 --country_code_index 9 ../2013-shp1.shp ~/Projects/wie/wie-web/app/assets/javascripts/2013.js
+
+python2 converter.py --width 1000 --viewport "-41.6 25.1 60.3 71.4" --longitude0 11.4 --name 1994 --projection mill --language en --country_name_index 1 --country_code_index 4 ../1994-shp1.shp ~/Projects/wie/wie-web/app/assets/javascripts/1994.js
+
+python2 converter.py --width 1000 --viewport "-41.6 25.1 60.3 71.4" --longitude0 11.4 --name 1945 --projection mill --language en --country_name_index 1 --country_code_index 4 ../1945-shp1.shp ~/Projects/wie/wie-web/app/assets/javascripts/1945.js
