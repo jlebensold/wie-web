222e7d63ffdb60923789c32fe7a3612d230ba96d
Jon Lebensold <jon@lebensold.ca>
2014-02-12 01:10:03 +0100

    minor styles

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
+    #invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
 
     to :launch do
